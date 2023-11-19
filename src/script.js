99e48b256bec407df0a214e5abcf318093c42902
Ameer <ameerapex496@gmail.com>
2023-11-19 09:37:28 +0100

    added function to navbar buttons

--- src/script.js
+++ src/script.js
@@ -1,1 +1,21 @@
-console.log("hallo world");
+// getting our elemetns
+var openMenu = document.querySelector(".open-Menu");
+var closeMenu = document.querySelector(".close-Menu");
+var navbar = document.querySelector(".navbar");
+// here is our global varibale
+// here is our functions
+// open menu func
+var openMenuHandler = function () {
+    if (!navbar.classList.contains("active")) {
+        navbar.classList.add("active");
+    }
+};
+// close menu func
+var closeMenuHandler = function () {
+    if (navbar.classList.contains("active")) {
+        navbar.classList.remove("active");
+    }
+};
+// here is our event lisnters
+openMenu.addEventListener("click", openMenuHandler);
+closeMenu.addEventListener("click", closeMenuHandler);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,27 @@
-console.log("hallo world");
+// getting our elemetns
+const openMenu = document.querySelector(".open-Menu") as HTMLElement;
+const closeMenu = document.querySelector(".close-Menu") as HTMLElement;
+const navbar = document.querySelector(".navbar") as HTMLElement;
+// here is our global varibale
+
+// here is our functions
+
+// open menu func
+
+const openMenuHandler: any = () => {
+  if (!navbar.classList.contains("active")) {
+    navbar.classList.add("active");
+  }
+};
+
+// close menu func
+
+const closeMenuHandler: any = () => {
+  if (navbar.classList.contains("active")) {
+    navbar.classList.remove("active");
+  }
+};
+
+// here is our event lisnters
+openMenu.addEventListener("click", openMenuHandler);
+closeMenu.addEventListener("click", closeMenuHandler);
